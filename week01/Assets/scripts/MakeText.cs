fb02bdf263e5e43d03491b2449d685dc2d23d51b
Noah Lemen <noah.lemen@gmail.com>
2014-01-29 16:02:10 -0500

    sculpture & text

---
+++ week01/Assets/scripts/MakeText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeText : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<TextMesh>().text = "YOOOO";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position -= Vector3.up * Mathf.Sin(Time.time*15f)/10f;
+	}
+}
---
+++ week01/Assets/scripts/rotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("HELLOOOO");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * Time.deltaTime *10f);
+	}
+}
