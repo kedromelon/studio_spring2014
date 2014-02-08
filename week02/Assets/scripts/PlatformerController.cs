6be6c237019401b58ebf6f6d94ca5aa47a66757d
Noah Lemen <noah.lemen@gmail.com>
2014-02-08 16:49:07 -0500

    fixed public variables for controller

--- week02/Assets/scripts/PlatformerController.cs
+++ week02/Assets/scripts/PlatformerController.cs
@@ -11,6 +11,8 @@ public class PlatformerController : MonoBehaviour {
 	public float speed = 1f;
 	public float jumpSpeed = 10f;
 	public float acceleration = 5f;
+	public float airacceleration = 2f;
+	public float turnspeed = 5f;
 
 	// Use this for initialization
 	void Start () {
@@ -32,20 +34,23 @@ public class PlatformerController : MonoBehaviour {
 		if (Input.GetKey(KeyCode.D))
 			moveVector += Vector3.right;
 
+		if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)) && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+			moveVector *= 0.707f;
+
 		if (controller.isGrounded){
 			if (Input.GetKeyDown(KeyCode.Space))
 				velocity.y = jumpSpeed;
 			velocity.x = Mathf.Lerp(velocity.x, moveVector.x*speed, Time.deltaTime * acceleration);
 			velocity.z = Mathf.Lerp(velocity.z, moveVector.z*speed, Time.deltaTime * acceleration);
 		}else{
-			velocity.x = Mathf.Lerp(velocity.x, moveVector.x*speed, Time.deltaTime * acceleration / 2f);
-			velocity.z = Mathf.Lerp(velocity.z, moveVector.z*speed, Time.deltaTime * acceleration / 2f);
+			velocity.x = Mathf.Lerp(velocity.x, moveVector.x*speed, Time.deltaTime * airacceleration);
+			velocity.z = Mathf.Lerp(velocity.z, moveVector.z*speed, Time.deltaTime * airacceleration);
 		}
 
 		if (velocity.x != 0f || velocity.z != 0f)
 			transform.rotation = Quaternion.Slerp(transform.rotation, 
 			                                      Quaternion.LookRotation(new Vector3(velocity.x, 0f, velocity.z)),
-			                                      Time.deltaTime*5f);
+			                                      Time.deltaTime * turnspeed);
 			
 		velocity.y -= gravity * Time.deltaTime;
 		controller.Move(velocity * Time.deltaTime);
@@ -53,6 +58,7 @@ public class PlatformerController : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(){
+		// reset to starting point
 		transform.position = new Vector3(0f,2f,0f);
 	}
 }
