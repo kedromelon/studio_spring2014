e9563ad654e12ac5289561c70de7ced9c1ce9be6
Noah Lemen <noah.lemen@gmail.com>
2014-02-10 16:11:19 -0500

    weed 3, 10print port

---
+++ week03/Assets/scripts/tenprint.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tenprint : MonoBehaviour {
+
+	public TextMesh mytext;
+	int charkey;
+	int index = 0;
+	public int charsinline = 50;
+
+	// Use this for initialization
+	void Start () {
+		mytext = GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		charkey = Random.Range(0,2);
+		switch (charkey){
+			case (0):
+				charkey = 47;
+				break;	
+			case (1):
+				charkey = 92;
+				break;
+		}
+
+		mytext.text+=(char)charkey;
+
+		index++;
+		if ( index == charsinline ){
+			index = 0;
+			mytext.text += "\n";
+		}
+
+	}
+}
