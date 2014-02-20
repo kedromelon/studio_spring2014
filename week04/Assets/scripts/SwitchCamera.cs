138354dfee74183bdefdb5d1db1abef54b0e3407
Noah Lemen <noah.lemen@gmail.com>
2014-02-20 17:33:31 -0500

    week 4, rigidbody rube goldberg machine

---
+++ week04/Assets/scripts/SwitchCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchCamera : MonoBehaviour {
+
+	public GameObject fromcamera, tocamera;
+
+	void OnTriggerEnter(){
+		fromcamera.camera.enabled = false;
+		tocamera.camera.enabled = true;
+	}
+}
