7c2e273d6a48d19585017eb93b2f4e804b07eec7
Noah Lemen <noah.lemen@gmail.com>
2014-02-24 17:19:59 -0500

    week 5; wanderer; god

---
+++ week05/Assets/God.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class God : MonoBehaviour {
+
+	public Wanderer blueprint;
+
+	List<Wanderer> FriendsList = new List<Wanderer>();
+
+	// Use this for initialization
+	void Start () {
+		int friendCount = 100;
+
+		for(int i = 0; i < friendCount; i++){
+			FriendsList.Add(SpawnFriend());
+		}
+	}
+
+	Wanderer SpawnFriend(){
+		Wanderer newFriend = 
+			Instantiate(blueprint, 
+		            new Vector3(Random.Range(-5f,5f),
+		            1f,
+		            Random.Range(-5f,5f)), 
+		            Quaternion.Euler(0f, Random.Range(0f,360f),0f)) as Wanderer;
+
+		return newFriend;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)){
+			foreach(Wanderer friend in FriendsList){
+				friend.destination = new Vector3(0f,0f,0f);
+			}
+		}
+
+		if (Input.GetMouseButton(0)){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit rayHit = new RaycastHit();
+			
+			if (Physics.Raycast(ray, out rayHit, Mathf.Infinity)){
+				foreach(Wanderer friend in FriendsList){
+					friend.destination = rayHit.point;
+				}
+			}
+		}
+
+	}
+}
---
+++ week05/Assets/Wanderer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wanderer : MonoBehaviour {
+
+	public Vector3 destination = Vector3.zero;
+	public float wanderRange = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody>().AddForce(Vector3.Normalize(destination - transform.position), ForceMode.VelocityChange);
+
+		destination = new Vector3(Random.Range(-wanderRange,wanderRange),transform.position.y,Random.Range(-wanderRange,wanderRange));
+
+		if (Mathf.Abs(rigidbody.velocity.y) <= 0f){
+			rigidbody.velocity += transform.up * 10f;
+		}
+	}
+
+
+}
